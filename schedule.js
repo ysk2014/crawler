ffc8ae0922b6ec148ee28ce5c8ef5407b669af29
yinshikai <yinshikai@taihe.com>
2016-07-19 10:55:27 +0800

    bug

--- schedule.js
+++ schedule.js
@@ -4,7 +4,7 @@ var path = require('path');
 var schedule = require('node-schedule');
 
 var task = function() {
-	return schedule.scheduleJob('* 20 * * *', function() {
+	return schedule.scheduleJob('* 11 * * *', function() {
 
 		require(path.join(__dirname, 'app'));
 	});
