56f15f1669eb97cb9a4c4dab86557808dce986e0
yinshikai <yinshikai@taihe.com>
2016-07-29 15:02:20 +0800

    bug

--- test/email.js
+++ test/email.js
@@ -4,143 +4,71 @@ require(path.join(__dirname, '../global'));
 
 var email = require('../email');
 
-var data = [
-	{
-		"id": 13123,
-		"title": "绝地逃亡",
-		"info": {
-			"rating": "7.0",
-			"images": {
-				"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2367559705.jpg",
-				"large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2367559705.jpg",
-				"medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2367559705.jpg"
-			},
-			"casts": "伊利亚·伍德,伊利亚·伍德,伊利亚·伍德"
-		},
-		"data": [
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
+var data = [ { mv: 
+     { images: '{"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2361744534.jpg","large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2361744534.jpg","medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2361744534.jpg"}',
+       rating: '6.6',
+       casts: '季冠霖,苏尚卿,许魏洲,金士杰' },
+    id: 3,
+    mid: 5045678,
+    title: '大鱼海棠',
+    year: '2016',
+    type: 0,
+    results: null,
+    addtime: '1469518958',
+    data: [ { from: 'bttt', sources: [ {
+    	title: '3243',
+    	href: 'dsadsadsa'
+    } ] } ],
+    error: [] },
+  { mv: 
+     { images: '{"small":"https://img1.doubanio.com/view/movie_poster_cover/ipst/public/p2361036748.jpg","large":"https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2361036748.jpg","medium":"https://img1.doubanio.com/view/movie_poster_cover/spst/public/p2361036748.jpg"}',
+       rating: '5.4',
+       casts: '包贝尔,宋佳,朱亚文,焦俊艳' },
+    id: 7,
+    mid: 25849006,
+    title: '陆垚知马俐',
+    year: '2016',
+    type: 0,
+    results: null,
+    addtime: '1469518958',
+    data: [ { from: 'bttt', sources: [ {
+    	title: '3243',
+    	href: 'dsadsadsa'
+    } ] } ],
+    error: [] },
+  { mv: 
+     { images: '{"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2358403173.jpg","large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2358403173.jpg","medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2358403173.jpg"}',
+       rating: '5.5',
+       casts: '李敏镐,钟汉良,唐嫣,徐正曦' },
+    id: 16,
+    mid: 21941770,
+    title: '赏金猎人',
+    year: '2016',
+    type: 0,
+    results: null,
+    addtime: '1469518959',
+    data: [ { from: 'bttt', sources: [ {
+    	title: '3243',
+    	href: 'dsadsadsa'
+    } ] } ],
+    error: [] },
+  { mv: 
+     { images: '{"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2366570716.jpg","large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2366570716.jpg","medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2366570716.jpg"}',
+       rating: '7.8',
+       casts: '陈永忠,谢理循,余世学,郭月' },
+    id: 26,
+    mid: 26337866,
+    title: '路边野餐',
+    year: '2015',
+    type: 0,
+    results: null,
+    addtime: '1469638805',
+    data: [ { from: 'bttt', sources: [ {
+    	title: '3243',
+    	href: 'dsadsadsa'
+    } ] } ],
+    error: [] } ];
 
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			}
-		]
-	},
-		{
-		"id": 13123,
-		"title": "绝地逃亡",
-		"info": {
-			"rating": "7.0",
-			"images": {
-				"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2367559705.jpg",
-				"large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2367559705.jpg",
-				"medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2367559705.jpg"
-			},
-			"casts": "伊利亚·伍德,伊利亚·伍德,伊利亚·伍德"
-		},
-		"data": [
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			}
-		]
-	},
-		{
-		"id": 13123,
-		"title": "绝地逃亡",
-		"info": {
-			"rating": "7.0",
-			"images": {
-				"small":"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2367559705.jpg",
-				"large":"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2367559705.jpg",
-				"medium":"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2367559705.jpg"
-			},
-			"casts": "伊利亚·伍德,伊利亚·伍德,伊利亚·伍德"
-		},
-		"data": [
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			},
-			{
-				"from": "bttt",
-				"source": [
-					{
-						"title": "绝地逃亡",
-						"href": "http://walle.taihenw.com/task/index"
-					}
-
-				]
-			}
-		]
-	},
-];
 
 
 http.createServer(function(req, res) {
