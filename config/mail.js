889c7e5d28ac78d937852f2661bf5ef6f95c33f1
yinshikai <yinshikai@taihe.com>
2016-10-10 17:01:44 +0800

    bug

--- config/mail.js
+++ config/mail.js
@@ -2,5 +2,5 @@
 module.exports = {
   	username: '1181102772@qq.com',
   	password: 'yinshikai2015',
-  	to: ['1181102772@qq.com','1185534172@qq.com']
+  	to: ['1181102772@qq.com','1185534172@qq.com','2281951103@qq.com','113916078@qq.com','2409551912@qq.com','2267176944@qq.com']
 }
