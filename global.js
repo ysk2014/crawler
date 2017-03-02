1677f2033b118f53a93e2967507cdd5864dc43b5
yinshikai <yinshikai@taihe.com>
2017-03-02 16:29:16 +0800

    bug

--- global.js
+++ global.js
@@ -56,8 +56,8 @@ global.mapLimit = function(coll, limit, iteratee, callback) {
 		}
 	}
 
-	var _len = limit>coll.length ? limit : coll.length
-	for (let i = 0; i < _len; i++) {
+	var _len = limit>coll.length ? coll.length : limit
+	for (var i = 0; i < _len; i++) {
 		fun(coll[i]);
 	}
 }
