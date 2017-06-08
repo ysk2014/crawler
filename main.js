c1edba1366d52042ba7777c8531b191cae0cf441
ysk2014 <1181102772@qq.com>
2017-06-08 22:46:17 +0800

    bug

--- main.js
+++ main.js
@@ -9,28 +9,24 @@ var sourceModel = require(path.join(__dirname, 'models/schedule'));
 
 var App = path.join(ROOT,'app');
 
-function main() {
+async function main() {
 	console.log('程序开始，获取定时数据');
 
-	sourceModel.getAll().then(function(source) {
-
-		var data = {};
-		source.forEach(function(item) {
-			if (data[item.type]) {
-				data[item.type].push(item);
-			} else {
-				data[item.type] = [item];
-			}
-		});
-		
-		for (var d in data) {
-			var ctrl = require(path.join(App,d));
-			ctrl(data[d]);
+	var source = await sourceModel.getAll();
+	
+	var data = {};
+	source.forEach(function(item) {
+		if (data[item.type]) {
+			data[item.type].push(item);
+		} else {
+			data[item.type] = [item];
 		}
-
-	}).catch(function(err) {
-		console.error(err.stack || err);
 	});
+	
+	for (var d in data) {
+		var ctrl = require(path.join(App,d));
+		ctrl(data[d]);
+	}
 }
 
 
