e82564bccdbcfadf0ddc085f5f0ce3dda747e050
ysk2014 <1181102772@qq.com>
2017-06-12 21:38:41 +0800

    bug

--- app/novel/index.js
+++ app/novel/index.js
@@ -7,9 +7,8 @@ var source = require(path.join(__dirname, './source'));
 
 module.exports = function(sources) {
     sources.forEach(function(item, i) {
-        source.start(item.title);
-        // schedule.scheduleJob(item.schedules, function() {
-        //     source.start(item.title);
-        // });
+        schedule.scheduleJob(item.schedules, function() {
+            source.start(item.title);
+        });
     }, this);
 }
